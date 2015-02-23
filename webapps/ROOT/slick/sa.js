9246fa39af5d1bf277dd2c040594e1b3d376e380
Giulia Del Bello <g.delbello@solvingteam.it>
2015-02-23 17:28:26 +0100

    bug

--- webapps/ROOT/slick/sa.js
+++ webapps/ROOT/slick/sa.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   $('.your-class').slick({
 	  infinite: true,
 	  adaptiveHeight: true,
-	  //autoplay: true,
-	  dots:true,
+	  autoplay: true,
+	  //dots:true,
 	  focusOnSelect:true,
 	  swipeToSlide:true
 	 	  
